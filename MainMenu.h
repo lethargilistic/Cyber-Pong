367acc1f804f4773504478bea4c0fafcf2163d98
IsemanTech <iceman1440k@gmail.com>
2016-04-11 23:07:29 -0400

    adding header file

--- MainMenu.h
+++ MainMenu.h
@@ -1,9 +1,21 @@
+#ifndef MAINMENU_H
+#define MAINMENU_H
 #include <SFML/Graphics.hpp>
+//#include "MainMenu.cpp"
 //Cyber Pong main menu
 class MainMenu
 {
+private:
+    int selectedItemIndex;
+    sf::Font font;
+    static const int Max_Items = 3;
+     sf::Text menu[Max_Items];
 public:
-    MainMenu();
+    MainMenu(int width, int height);
     ~MainMenu();
+    void draw(sf::RenderWindow &window);
+    void move_up();
+    void move_down();
 
 };
+#endif // MAINMENU_H
